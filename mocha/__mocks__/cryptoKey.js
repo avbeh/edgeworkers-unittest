875aee7ef334c4dd2da0889e0c023085d2a74461
avbeh <avbeh@akamai.com>
2022-09-21 12:53:27 -0400

    mocha_crypto_EW

--- mocha/__mocks__/cryptoKey.js
+++ mocha/__mocks__/cryptoKey.js
@@ -28,11 +28,13 @@ class Crypto {
 }
 export const crypto = new Crypto();
 
+//functions
 export const mock_verify_rs256 = sinon.stub();
-export const mock_pem2ab = sinon.stub();
-export const mock_encoding_base64url_decode = sinon.stub();
-
-export const pem2ab = mock_pem2ab;
 export const verify_rs256 = mock_verify_rs256;
+
+export const mock_pem2ab = sinon.stub();
+export const pem2ab = mock_pem2ab;
+
+export const mock_encoding_base64url_decode = sinon.stub();
 export const base64url_decode = mock_encoding_base64url_decode;
 
